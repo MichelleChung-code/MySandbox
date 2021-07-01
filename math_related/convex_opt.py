92567a91d1e02db96b93bd0a393392e7835e1409
mchung-code <tkd.m.chung@gmail.com>
2021-06-30 21:04:49 -0600

    Add example of how to retrieve dual variables

--- math_related/convex_opt.py
+++ math_related/convex_opt.py
@@ -6,13 +6,20 @@ x = cp.Variable()
 y = cp.Variable()
 obj = cp.Minimize(cp.square(x - y))
 constraints_ls = [-10 <= x, x <= 10, -10 <= y, y <= 10]
-
+# constraints_ls = [x + y == 1, x - y >= 1]
 prob = cp.Problem(obj, constraints_ls)
 
 # confirm that this is convex
 assert prob.is_dcp()  # will return True if it is a valid convex
 
 prob.solve()
+
+# print out the optimal dual variables too
+# dual vars (langrange multipliers) for a constraint are in constraint.dual_value
+# dual problem is to maximize the obj func w.r.t the dual variables under derived constraints on the dual variables
+
+for i, item in enumerate(constraints_ls):
+    print(f'optimal {item} dual variable: {constraints_ls[i].dual_value}')
 
 
 def obj_func(tup_x_y):
