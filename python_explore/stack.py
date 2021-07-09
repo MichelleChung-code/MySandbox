167340a556024b62fbe10c5868eb4bc9defbcc7e
mchung-code <tkd.m.chung@gmail.com>
2021-07-09 10:10:47 -0600

    add stack class

---
+++ python_explore/stack.py
@@ -0,0 +1,29 @@
+class Stack:
+    def __init__(self):
+        self.elements = []
+
+    def is_empty(self):
+        return len(self.elements) == 0
+
+    def push(self, elem):
+        self.elements.append(elem)
+
+    def pop(self):
+        return self.elements.pop()
+
+    def peek(self):
+        # see what the last item is but don't actually remove it
+        return self.elements[-1]
+
+    def size(self):
+        return len(self.elements)
+
+    def __repr__(self):
+        return f'{self.__class__.__name__}({self.elements})'
+
+if __name__ == '__main__':
+    s = Stack()
+    s.push('blah')
+    print(s)
+    print(s.peek())
+
