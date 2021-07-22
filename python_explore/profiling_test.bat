c4c9fe8411affc05fc59e5ab51f1d887fbd38c14
mchung-code <tkd.m.chung@gmail.com>
2021-07-22 12:33:38 -0400

    Add how to call using the memory_profile

--- python_explore/profiling_test.bat
+++ python_explore/profiling_test.bat
@@ -1,5 +1,8 @@
-@REM Save the commands for running line profiler
+@REM Save the commands for running line_profiler
 @REM Have the profile decorator in prof.py file
 
 kernprof -l prof.py
 python -m line_profiler prof.py.lprof 
+
+@REM For using memory_profiler (prof.py also needs to have the profile decorator here)
+python -m memory_profiler prof.py
