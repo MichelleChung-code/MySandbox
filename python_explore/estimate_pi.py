87511d700b1ebabad7dcb551b50c441e2494b1e4
mchung-code <tkd.m.chung@gmail.com>
2021-08-10 11:40:16 -0400

    Add option to vectorize using numpy

--- python_explore/estimate_pi.py
+++ python_explore/estimate_pi.py
@@ -2,6 +2,9 @@ from timing_dec import timing
 import multiprocessing as mp
 
 import random
+import numpy as np
+
+
 # multiprocessing internally seeds when it sees the python random package in the namespace
 # however, if using the numpy random module... need to make sure to explicitly seed.
 
@@ -25,8 +28,27 @@ def calc_inside_quarter_circ(num_experiments):
     return res
 
 
+def calc_inside_quarter_circ_np(num_experiments):
+    """
+    Return the number of tries that ended up inside the area of the quarter circle
+    Version of calc_inside_quarter_circ but vectorizing with numpy
+
+    Args:
+        num_experiments: <int> or <float> number of trials to run.  If <float>, will be converted to <int>
+
+    Returns:
+        <int> number of tries that ended up in the quarter circle
+
+    """
+    np.random.seed()  # explicitly set seed to ensure that each of the forked processes generate a unique sequence of random numbers
+
+    x = np.random.uniform(0, 1, int(num_experiments))
+    y = np.random.uniform(0, 1, int(num_experiments))
+    return np.sum(x ** 2 + y ** 2 <= 1)
+
+
 @timing
-def estimate_pi_multiprocess(num_experiments):
+def estimate_pi_multiprocess(num_experiments, use_numpy=False):
     """
     Estimates the value of pi - monte carlo randomized experiments
 
@@ -42,11 +64,14 @@ def estimate_pi_multiprocess(num_experiments):
     worker_samples = num_experiments / num_cores
     per_process = [worker_samples] * num_cores
 
-    pi = sum(pool.map(calc_inside_quarter_circ, per_process)) * 4 / num_experiments
+    quarter_circ_res = pool.map(calc_inside_quarter_circ, per_process) if not use_numpy else pool.map(
+        calc_inside_quarter_circ_np, per_process)
+    pi = sum(quarter_circ_res) * 4 / num_experiments
 
     return pi
 
 
 if __name__ == '__main__':
     num_experiments = 10_000_000
-    print(estimate_pi_multiprocess(num_experiments))
+    print(estimate_pi_multiprocess(num_experiments))  # does not use numpy
+    print(estimate_pi_multiprocess(num_experiments, use_numpy=True))  # vectorizes with numpy
