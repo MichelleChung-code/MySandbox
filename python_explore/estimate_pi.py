da7a47e0a77d8087b3b0baabd40359bba9a47392
mchung-code <tkd.m.chung@gmail.com>
2021-08-09 21:05:12 -0400

    multiprocessing pi example

---
+++ python_explore/estimate_pi.py
@@ -0,0 +1,50 @@
+from timing_dec import timing
+import multiprocessing as mp
+import random
+
+
+def calc_inside_quarter_circ(num_experiments):
+    """
+    Return the number of tries that ended up inside the area of the quarter circle
+
+    Args:
+        num_experiments: <int> or <float> number of trials to run.  If <float>, will be converted to <int>
+
+    Returns:
+        <int> number of tries that ended up in the quarter circle
+
+    """
+    res = 0
+    for i in range(int(num_experiments)):
+        x = random.uniform(0, 1)
+        y = random.uniform(0, 1)
+        res += (x ** 2 + y ** 2 <= 1)  # adds 1 for the bool True condition
+
+    return res
+
+
+@timing
+def estimate_pi_multiprocess(num_experiments):
+    """
+    Estimates the value of pi - monte carlo randomized experiments
+
+    Args:
+        num_experiments: <int> total number of experiments to run across all cores
+
+    Returns:
+        <float> the estimated value for pi
+    """
+    num_cores = mp.cpu_count()
+    pool = mp.Pool(processes=num_cores)
+
+    worker_samples = num_experiments / num_cores
+    per_process = [worker_samples] * num_cores
+
+    pi = sum(pool.map(calc_inside_quarter_circ, per_process)) * 4 / num_experiments
+
+    return pi
+
+
+if __name__ == '__main__':
+    num_experiments = 10_000_000
+    print(estimate_pi_multiprocess(num_experiments))
