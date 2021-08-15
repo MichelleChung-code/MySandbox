a1a53f2e2e70ee0204b1aa22eeebbe1eff500089
mchung-code <tkd.m.chung@gmail.com>
2021-08-14 20:57:27 -0400

    Add assertations for tensor dot product and contraction

--- math_related/tensor_product.py
+++ math_related/tensor_product.py
@@ -21,20 +21,38 @@ def tensor_prod(A, B):
 
 
 def tensor_dot_prod(A, B):
-    # add code for the assertation
     res = np.tensordot(A, B, axes=1)
+
+    assert res == A @ B
+
     return res
 
 
 def tensor_double_contraction(A, B):
-    # add code for assertation
+    # element-wise multiplication and then sum
+    manual_res = np.multiply(A, B).sum()
+
     res = np.tensordot(A, B, axes=2)  # axes = 2 is also the default case
+
+    assert res == manual_res
+
     return res
 
 
 if __name__ == '__main__':
     # Just trying out vectors right now
     A = np.array([1, 2, 3])
-    B = np.array([4, 5])
+    B = np.array([4, 5, 6])
 
     print(tensor_prod(A, B))
+    print(tensor_dot_prod(A, B))
+
+    C = np.array([[1, 2, 3],
+                  [4, 2, 2],
+                  [2, 3, 4]])
+
+    D = np.array([[1, 4, 7],
+                  [2, 5, 8],
+                  [3, 6, 9]])
+
+    print(tensor_double_contraction(C, D))
